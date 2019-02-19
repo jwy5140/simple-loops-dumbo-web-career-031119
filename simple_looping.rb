370f38535442a0f151e241bc9a65cd07d90e1b10
Jeffrey Yu <jwy5140@gmail.com>
2019-02-19 05:26:08 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,10 @@ def loop_iterator(number_of_times)
   
   loop do
     puts phrase
-    number_of_times == 0 ? break : number_of_times -= 1
+    number_of_times -= 1 
+    if (number_of_times == 0)
+      break
+    end
   end
 end
 
